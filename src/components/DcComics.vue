9b6d3a5db7382b721be231378aa648da484f5d2c
Mattia Bardino <Mattia.errol@gmail.com>
2022-02-21 15:59:16 +0100

    prime input

---
+++ src/components/DcComics.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>DcComics</h1>
+</template>
+
+<script>
+export default {
+  name: 'DcComics',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
