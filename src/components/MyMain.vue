20f62f645f16712ac8664803ce11658e37130c68
Mattia Bardino <Mattia.errol@gmail.com>
2022-02-23 01:41:06 +0100

    ,

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -3,6 +3,61 @@
     <section class="comics">
         <div class="container">
 
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+            
+            <div class="long-card">
+                <a href="#">
+                    <div class="current-series">
+                            <h4>LOAD MORE</h4>
+                    </div>
+                </a>
+            </div>
         </div>
     </section>
  
@@ -20,13 +75,41 @@ export default {
 @import "src/assets/style/variables.scss";
 
 .comics {
-    height: 500px;
+    height: 600px;
     background-color: rgb(28, 28, 28);
     .container {
         height: 100%;
         width: 80%;
-        border: 1px solid white;
         margin: auto;
+        .card {
+            height: calc(80% / 2);
+            width: calc(100% / 6);
+            border: 1px solid white;
+            float: left;
+        }
+        .long-card {
+            height: 20%;
+            width: 100%;
+            float: left;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+                a {
+                    text-decoration: none;
+                }
+              .current-series {
+                background-color: $primaryColor;
+                width: 190px;
+                padding: 10px;
+                text-align: center;
+                position: relative;
+                top: 25px;
+                    h4 {
+                        color: white;
+                        font-size: 12px;
+                    }
+            }
+        }
     }
 }
 
